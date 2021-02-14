00107bcbdd223fe588d55e6cd1b437aa6b886071
Raghavendra P <444raghav@gmail.com>
2021-02-14 23:01:49 +0530

    bottom_navigation

--- qatar_care/lib/screens/main_screen.dart
+++ qatar_care/lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qatar_care/base/basic_scaffold.dart';
 
+import 'guest2.dart';
 import 'guest_screen.dart';
 
 class MainScreen extends StatefulWidget {
@@ -9,20 +10,22 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex=2;
+  Widget pages=GuestScreen();
   @override
   Widget build(BuildContext context) {
     final _height = MediaQuery.of(context).size.height;
     final _width = MediaQuery.of(context).size.width;
     final _fontstyles = 'Roboto sans-serif';
-    return BasicScaffold(
-      child: Container(
+    return Scaffold(
+      body: Container(
         padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         width: double.infinity,
         color: Color(0xFF7EAF3C),
         child: Column(
           children: <Widget>[
             Container(
-              padding:EdgeInsets.only(left:20,right:20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               width: double.infinity,
               height: _height * 0.1,
               child: Row(
@@ -62,12 +65,59 @@ class _MainScreenState extends State<MainScreen> {
                 ),
                 width: double.infinity,
                 height: double.infinity,
-                child: GuestScreen(),
+                child: SingleChildScrollView(child:pages),
               ),
             ),
           ],
         ),
       ),
+      bottomNavigationBar: Container(
+        height: _height * 0.1,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              BottomNavTile(Icons.home_outlined, 0,GuestScreen()),
+              BottomNavTile(Icons.dock, 1,Myapp()),
+              BottomNavTile(Icons.search_off_outlined, 2,GuestScreen()),
+              BottomNavTile(Icons.person_outline_outlined, 3,GuestScreen()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget BottomNavTile(IconData icon, int index,Widget Pages) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedIndex = index;
+          pages=Pages;
+        });
+      },
+      child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: index == _selectedIndex ? Color(0xff7EAF3C) : Colors.white,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Icon(
+            icon,
+            color: index == _selectedIndex ? Colors.white: Color(0xFF989BA5) ,
+          )),
     );
   }
 }
